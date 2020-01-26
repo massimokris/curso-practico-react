23630a683518a8e35f900f18caa1e8a199de9464
Massimo Di Berardino <maxdo1996@gmail.com>
2020-01-26 04:15:22 -0300

    final

---
+++ platziVideo/src/containers/Player.jsx
@@ -0,0 +1,43 @@
+import React, { useLayoutEffect } from "react";
+import { connect } from "react-redux";
+import { getVideoSource } from "../actions/index";
+import NotFound from "./NotFound";
+
+//Styles
+import "../assets/styles/components/Player.scss";
+
+const Player = props => {
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useLayoutEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
+    <section className="Player">
+      <video controls autoPlay>
+        <source src={props.playing.source} type="video/mp4" />
+      </video>
+      <section className="Player-back">
+        <button type="button" onClick={() => props.history.goBack()}>
+          Regresar
+        </button>
+      </section>
+    </section>
+  ) : (
+    <NotFound />
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    playing: state.playing
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
---
+++ platziVideo/src/utils/gravatar.js
@@ -0,0 +1,11 @@
+import md5 from 'md5';
+
+const gravatar = (email) => {
+    const base = 'https://gravatar.com/avatar/';
+    const formattedEmail = (email).trim().toLowerCase();
+    const hash = md5(formattedEmail, { endcoding: 'binary' });
+    console.log(hash);
+    return `${base}${hash}`;
+}
+
+export default gravatar;
